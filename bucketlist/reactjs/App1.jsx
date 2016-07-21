553b4c32de5314f60fced8992752243cd2dbd515
Chukwuerika Dike <chukwuerika.dike@andela.com>
2016-07-21 16:06:20 +0100

    [Chore] Add redux boilerplate

--- bucketlist/reactjs/App1.jsx
+++ bucketlist/reactjs/App1.jsx
@@ -1,14 +1,20 @@
 import React from "react"
 import { render } from "react-dom"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
 
 import App1Container from "./containers/App1Container"
+import store from "./store"
+
 
 class App1 extends React.Component {
-    render() {
-        return (
-            <App1Container />
-        )
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <App1Container />
+      </Provider>
+    )
+  }
 }
 
 render(<App1/>, document.getElementById('App1'))
--- bucketlist/reactjs/containers/App1Container.jsx
+++ bucketlist/reactjs/containers/App1Container.jsx
@@ -1,14 +1,31 @@
 import React from "react"
+import { connect } from "react-redux"
 
+import { increaseCounter } from "../actions/counterActions"
 import Headline from "../components/Headline"
 
-export default class App1Container extends React.Component {
+@connect((store) => {
+  return {
+    counters: store.counters,
+  };
+})
+
+export default class SampleAppContainer extends React.Component {
+  handleClick() {
+    this.props.dispatch(increaseCounter())
+  }
+
   render() {
+    console.log(this.props);
+    const { counters } = this.props;
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
-            <Headline>Hello World!</Headline>
+            <Headline>Sample App!</Headline>
+            <div onClick={this.handleClick.bind(this)}>INCREASE</div>
+            <p>{counters.clicks}</p>
+            <p>{process.env.BASE_API_URL}</p>
           </div>
         </div>
       </div>
